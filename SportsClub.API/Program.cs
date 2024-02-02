cc45260afeebe4c105aee203de3eaa6d49288f92
Paul de Mast <pjf.demast@avans.nl>
2024-02-02 17:16:57 +0100

    enable swagger again

--- SportsClub.API/Program.cs
+++ SportsClub.API/Program.cs
@@ -29,8 +29,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 // See: https://learn.microsoft.com/en-us/aspnet/core/security/cors#attr
 app.UseCors(policy =>
